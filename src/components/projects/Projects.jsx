83481f3cbc53b3f6019db8a4890b7fe9dc837be7
Abhay-1718 <abhaysingh80766@gmail.com>
2025-04-04 08:13:35 +0530

    change of link for the project

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -10,7 +10,7 @@ const Projects = () => {
       title: "CodeKami",
       description: "CodeKami is a cutting-edge, full-stack MERN (MongoDB, Express.js, React, Node.js) application designed to revolutionize the way developers review and refine their code. Powered by Google Gemini AI, CodeKami provides an intelligent, automated solution for reviewing code, offering deep insights and feedback to improve quality, performance, and maintainability",
       image: CodeKamiLogo,
-      link: "https://code-kami-frontend.vercel.app",
+      link: "https://codekami.online",
     },
     {
       number: "02",
